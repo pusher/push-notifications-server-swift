012c50b4c5f13ff7f17e17540874ca0a48f1a677
Luka Bratos <luka@pusher.com>
2019-01-22 17:23:29 +0000

    Refactor by using `publishToInterests`

--- Sources/PushNotifications/PushNotifications.swift
+++ Sources/PushNotifications/PushNotifications.swift
@@ -101,61 +101,27 @@ public struct PushNotifications {
     */
     @available(*, deprecated, renamed: "publishToInterests", message: "Use `publishToInterests` method.")
     public func publish(_ interests: [String], _ publishRequest: [String: Any], completion: @escaping (_ publishId: String) -> Void) throws {
-        if instanceId.isEmpty {
-            throw PushNotificationsError.instanceIdCannotBeAnEmptyString
-        }
+        do {
+            try publishToInterests(interests, publishRequest) { result in
+                switch result {
+                case .value(let deviceId):
+                    completion(deviceId)
+                case .error(let error):
+                    /**
+                     Communicationg errors by returning an empty string is not ideal.
+                     Better option would be to set the `publishId` as an optional
+                     or to return the proper error which is implemented in `publishToInterests` method.
 
-        if secretKey.isEmpty {
-            throw PushNotificationsError.secretKeyCannotBeAnEmptyString
-        }
-
-        if interests.isEmpty {
-            throw PushNotificationsError.interestsArrayCannotBeEmpty
-        }
-
-        if interests.count > 100 {
-            throw PushNotificationsError.interestsArrayContainsTooManyInterests(maxInterests: 100)
-        }
-
-        if !(interests.filter { $0.count > 164 }).isEmpty {
-            throw PushNotificationsError.interestsArrayContainsAnInvalidInterest(maxCharacters: 164)
-        }
-
-        let sessionConfiguration = URLSessionConfiguration.default
-        let session = URLSession.init(configuration: sessionConfiguration)
-
-        let urlString = "https://\(instanceId).pushnotifications.pusher.com/publish_api/v1/instances/\(instanceId)/publishes"
-        guard let url = URL(string: urlString) else { return }
-
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "POST"
-        urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
-        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.setValue("Bearer \(secretKey)", forHTTPHeaderField: "Authorization")
-
-        var mutablePublishRequest = publishRequest
-        mutablePublishRequest["interests"] = interests
-
-        urlRequest.httpBody = try? JSONSerialization.data(withJSONObject: mutablePublishRequest)
-        let dataTask = session.dataTask(with: urlRequest) { (data, response, error) in
-            guard
-                let data = data,
-                let httpURLResponse = response as? HTTPURLResponse
-                else {
-                    return // Improve
-            }
-
-            let statusCode = httpURLResponse.statusCode
-            guard statusCode >= 200 && statusCode < 300, error == nil else {
-                return // Improve
-            }
-
-            if let publishResponse = try? JSONDecoder().decode(PublishResponse.self, from: data) {
-                completion(publishResponse.id)
+                     This is a workaround so we don't break the API.
+                     **/
+                    print("[PushNotifications] - Publish request failed.")
+                    completion("")
+                }
             }
         }
-
-        dataTask.resume()
+        catch {
+            throw PushNotificationsError.error("[PushNotifications] - Publish request failed. Error: \(error)")
+        }
     }
 
     /**
