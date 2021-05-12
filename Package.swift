f2a6892a1afcaef9d6c887b6ad59e1f1a05054d5
Daniel Browne <daniel.browne@pusher.com>
2021-05-12 09:31:06 +0100

    Remove redundant comments from 'Package.swift'

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "PushNotifications",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "PushNotifications",
             targets: ["PushNotifications"])
@@ -20,8 +19,6 @@ let package = Package(
                  .upToNextMajor(from: "0.43.1")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PushNotifications",
             dependencies: ["SwiftJWT"]),
