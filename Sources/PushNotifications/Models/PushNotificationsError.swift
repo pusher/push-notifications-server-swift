e6d9c53a6f53225b3c7cf66ef05f5743229c9f90
Daniel Browne <daniel.browne@pusher.com>
2021-05-12 15:43:13 +0100

    Refactor 'PushNotificationsError'

--- Sources/PushNotifications/Models/PushNotificationsError.swift
+++ Sources/PushNotifications/Models/PushNotificationsError.swift
@@ -2,30 +2,30 @@ import Foundation
 
 /// Error thrown by PushNotifications.
 public enum PushNotificationsError: Error {
-    //// `instanceId` cannot be an empty String.
-    case instanceIdCannotBeAnEmptyString
-    //// `secretKey` cannot be an empty String.
-    case secretKeyCannotBeAnEmptyString
-    //// `interests` array cannot be empty.
-    case interestsArrayCannotBeEmpty
 
-    /**
-     General error.
-
-     - Parameter: error message.
-     */
+    /// A non-specific error.
+    /// - Parameter: The error message.
     case error(String)
 
-    /**
-    Interests array exceeded the number of maximum interests allowed.
+    /// The `instanceId` cannot be an empty String.
+    case instanceIdCannotBeAnEmptyString
 
-    - Parameter: maximum interests allowed value.
-    */
-    case interestsArrayContainsTooManyInterests(maxInterests: UInt)
-    /**
-    Interests array contains at least one or more invalid interests.
+    /// The `interests` array cannot be empty.
+    case interestsArrayCannotBeEmpty
 
-    - Parameter: maximum characters allowed value.
-    */
+    /// The `interests` array contains at least one or more invalid interests.
+    /// - maxCharacters: The maximum number of characters allowed.
     case interestsArrayContainsAnInvalidInterest(maxCharacters: UInt)
+
+    /// The `interests` array exceeded the number of maximum interests allowed.
+    /// - maxInterests: The maximum number of interests allowed.
+    case interestsArrayContainsTooManyInterests(maxInterests: UInt)
+
+    case internalError(_ error: Error)
+
+    /// The `secretKey` cannot be an empty String.
+    case secretKeyCannotBeAnEmptyString
+
+    /// The `userId` cannot be an empty String.
+    case userIdCannotBeAnEmptyString
 }
