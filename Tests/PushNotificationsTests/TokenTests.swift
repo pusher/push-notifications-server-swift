331366e09645392d1bea4e8e17ed841313759c41
Daniel Browne <daniel.browne@pusher.com>
2021-05-17 13:35:39 +0100

    Use new test `Result` verification API

--- Tests/PushNotificationsTests/TokenTests.swift
+++ Tests/PushNotificationsTests/TokenTests.swift
@@ -7,7 +7,7 @@ final class TokenTests: XCTestCase {
         let exp = XCTestExpectation(function: #function)
 
         TestObjects.Client.shared.generateToken(TestObjects.UserIDs.validId) { result in
-            self.verifyAPIResultSuccess(result, expectation: exp) { jwt in
+            self.verifyResultSuccess(result, expectation: exp) { jwt in
                 XCTAssertNotNil(jwt)
             }
         }
@@ -19,9 +19,9 @@ final class TokenTests: XCTestCase {
         let exp = XCTestExpectation(function: #function)
 
         TestObjects.Client.shared.generateToken(TestObjects.UserIDs.emptyString) { result in
-            self.verifyAPIResultFailure(result,
-                                        expectation: exp,
-                                        expectedError: .userIdCannotBeAnEmptyString)
+            self.verifyResultFailure(result,
+                                     expectation: exp,
+                                     expectedError: .userIdCannotBeAnEmptyString)
         }
 
         wait(for: [exp], timeout: 3)
@@ -31,9 +31,9 @@ final class TokenTests: XCTestCase {
         let exp = XCTestExpectation(function: #function)
 
         TestObjects.Client.shared.generateToken(TestObjects.UserIDs.tooLong) { result in
-            self.verifyAPIResultFailure(result,
-                                        expectation: exp,
-                                        expectedError: .userIdInvalid(maxCharacters: 164))
+            self.verifyResultFailure(result,
+                                     expectation: exp,
+                                     expectedError: .userIdInvalid(maxCharacters: 164))
         }
 
         wait(for: [exp], timeout: 3)
